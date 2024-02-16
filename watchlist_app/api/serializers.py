69824bcb130857bc2cc3c0b824e32ee9beaffbd4
harsh <harsh.saxena@thoughts2binary.com>
2024-02-16 17:34:02 +0530

    pushing code for validators

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,7 +2,21 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 
+# validators
+def name_length(value):
+    if len(value) < 2:
+        raise serializers.ValidationError("name is too short!")
+    else:
+        return value
+
+
 class MovieSerializer(serializers.ModelSerializer):
+
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(validators=[name_length])
+    description = serializers.CharField()
+    is_active = serializers.BooleanField()
+
     class Meta:
         model = Movie
         fields = '__all__'
@@ -16,3 +30,19 @@ class MovieSerializer(serializers.ModelSerializer):
         instance.is_active = validated_data.get('is_active', instance.is_active)
         instance.save()
         return instance
+
+    # validators
+    # # field level validation
+    # def validate_name(self, value):
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError("name is too short!")
+    #     else:
+    #         return value
+
+    # object level validation
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("name and description cannot be same!")
+        else:
+            return data
+
